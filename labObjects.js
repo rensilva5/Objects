7b4a834c9f8556bd4634d4d838680813e4b2b001
Renzo Silva <renzoasilva@gmail.com>
2022-07-14 11:45:38 -0400

    Learn about objects

---
+++ labObjects.js
@@ -0,0 +1,34 @@
+// let studentInfo = {
+//     name: 'Renzo',
+//     age: 10,
+//     inClass: true
+// }
+
+// console.log(studentInfo)
+// console.log(studentInfo.name)
+
+// studentInfo.age = 20
+// console.log(studentInfo.age)
+
+//Lab2
+
+// let teacher = {
+//     name: "Jhon",
+//     classes: [],
+//     currentlyTeaching: true
+// }
+// teacher.classes.push('Maths')
+// teacher.classes.push('Arrays')
+
+// console.log(teacher.name)
+// console.log(teacher.classes)
+// console.log(teacher)
+
+TeacherAssistant = {
+    name: "Paula",
+    inClass: true,
+}
+console.log(TeacherAssistant)
+TeacherAssistant.Classes = []
+TeacherAssistant.Classes.push("Looping")
+console.log(TeacherAssistant)
---
+++ objects.js
@@ -0,0 +1,36 @@
+
+let person = {
+    name: "Renzo",
+    lastName: "/silva",
+    age: 15,
+    inClass: true,
+    classesTaught: ['javascript', 'arrays']
+
+}
+person.hasGlasses = true  //Add new property
+console.log(person)
+
+person.name = 'Peter Pan' //edit the object
+console.log(person)
+
+
+// const allProps = Object.keys(person)
+// console.log(allProps)
+// const hasAge = allProps.indexOf('age') !=-1
+// console.log(hasAge)
+
+//console.log(person.name)
+//delete person.name
+
+// let {name, age} = person
+// console.log(name)
+// console.log(age)
+
+// console.log(person.classesTaught)
+// console.log(person.classesTaught[1])
+// console.log(person['classesTaught'][0])
+// console.log(person['name'])
+
+// person.name = 'RenzS'
+// console.log(person.name,)
+
