fb81cf79ee85e663134ebea4661aa06173e051a8
Renzo Silva <renzoasilva@gmail.com>
2022-07-24 11:45:22 -0400

    New description in objects

--- labObjects.js
+++ labObjects.js
@@ -1,34 +1,37 @@
-// let studentInfo = {
-//     name: 'Renzo',
-//     age: 10,
-//     inClass: true
-// }
+let studentInfo = {
+    name: 'Renzo',
+    age: 10,
+    inClass: true
+}
 
-// console.log(studentInfo)
-// console.log(studentInfo.name)
+console.log(studentInfo)      //prints out my info
+console.log(studentInfo.name) // prints out only my name
 
-// studentInfo.age = 20
-// console.log(studentInfo.age)
+studentInfo.age = 20        // Changes age 10 to age 20
+console.log(studentInfo.age)  //Prints out 20
 
-//Lab2
+// --- Lab2
 
-// let teacher = {
-//     name: "Jhon",
-//     classes: [],
-//     currentlyTeaching: true
-// }
-// teacher.classes.push('Maths')
-// teacher.classes.push('Arrays')
+let teacher = {
+    name: "Jhon",
+    classes: [],
+    currentlyTeaching: true
+}
+console.log(teacher)      // teacher's info
+teacher.classes.push('Maths')  // add maths to classes inside the array
+teacher.classes.push('Arrays') // add arrays into classes inside the array
 
-// console.log(teacher.name)
-// console.log(teacher.classes)
-// console.log(teacher)
+console.log(teacher.name)  //prints out Jhon
+console.log(teacher.classes) // prints out classes recently added (maths, arrays)
+console.log(teacher)  // prints out teacher with updated info.  
+
+// -----
 
 TeacherAssistant = {
     name: "Paula",
     inClass: true,
 }
-console.log(TeacherAssistant)
-TeacherAssistant.Classes = []
-TeacherAssistant.Classes.push("Looping")
-console.log(TeacherAssistant)
+console.log(TeacherAssistant)  // prints teacherAssistant into => Paula and true
+TeacherAssistant.Classes = []  // adds a new property with an array as value
+TeacherAssistant.Classes.push("Looping")  // adds or push "looping" inside the array in classes as new value
+console.log(TeacherAssistant) // prints out updated info including new classes property with its new value ("looping")
